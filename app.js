6e06f3a7e563be36e85a6fbb183ad53339127f4e
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-01-03 03:58:30 +0100

    update contact string.

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Welcome to my micro-blog/journal site! This is a place where I share my thoughts and experiences as a newcomer to the tech industry. You can expect to find posts about the challenges and triumphs I encounter as I learn and grow in this exciting field. I hope that my journey can provide insight and inspiration to others who are also just starting out in tech. Thank you for visiting and I hope you enjoy reading my posts.";
+const aboutContent = "Hello and welcome to my site! My name is Fortune and I am a Web App developer. I am passionate about technology and love sharing my thoughts and experiences through writing. On this site, you will find a collection of micro-blogs, journal entries and articles that cover a range of topics, from web app developments to med tech. I hope that my words will provide a fresh perspective and inspire others to think more deeply about the world around us. Thank you for taking the time to visit and I hope you find something that resonates with you.";
+const contactContent = "I would love to hear from you! Whether you have a question, comment, or just want to connect, don't hesitate to reach out to me. I'm always open to meeting new people and having thoughtful conversations. You can find my contact information at the bottom of this page. Don't be a stranger, I can't wait to hear from you!";
+const twitter='https://twitter.com/EwuruFortune/'
 
 const app = express();
 
@@ -71,7 +72,7 @@ app.get("/about", function(req, res){
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact", {contactContent: contactContent,twitter:twitter});
 });
 
 
